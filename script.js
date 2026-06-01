eddbb3cc35fe954672e31d1ab63cdcd8f9e303cf
manwithcolt <marthagen@gmail.com>
2026-06-02 00:10:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,16 +15,30 @@ async function loadBikes() {
     }
 
     const container = document.getElementById("bikes");
-    container.innerHTML = "";
+    container.innerHTML += `
+<div class="bike">
+    <h3>${b.model}</h3>
 
-    bikes.forEach(b => {
-        container.innerHTML += `
-            <div class="bike">
-                <h3>${b.model}</h3>
-                <p>${b.dealer}</p>
-                <p>${b.price} €</p>
-                <a href="${b.url}" target="_blank">Zum Angebot</a>
-            </div>
+    <p><b>Händler:</b> ${b.dealer}</p>
+    <p><b>Kategorie:</b> ${b.category}</p>
+    <p><b>Rahmen:</b> ${b.frame}</p>
+    <p><b>Schaltung:</b> ${b.groupset}</p>
+    <p><b>Gewicht:</b> ${b.weight}</p>
+    <p><b>Laufräder:</b> ${b.wheelset}</p>
+
+    <p>
+      <b>${b.price} €</b>
+      statt ${b.old_price} €
+      (-${b.discount}%)
+    </p>
+
+    <p><b>Größen:</b> ${b.sizes.join(", ")}</p>
+
+    <a href="${b.url}" target="_blank">
+      Zum Angebot
+    </a>
+</div>
+`;
         `;
     });
 }
